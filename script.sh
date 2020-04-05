081a5fda9d1741b95ec563a25765e744493667d0
serhn <38658684+serhn@users.noreply.github.com>
2020-04-05 13:02:13 +0300

    Update script.sh
    asterisk 16

--- script.sh
+++ script.sh
@@ -1,18 +1,26 @@
 #!/bin/sh
 apt-get install -y asterisk asterisk-dev
 cd /usr/src/
-wget https://github.com/oleg-krv/asterisk-chan-dongle/archive/asterisk13.zip
 
-unzip asterisk13.zip
-rm -f asterisk13.zip
-cd asterisk-chan-dongle-asterisk13
-aclocal && autoconf && automake -a
-./configure
+wget https://github.com/haha8x/asterisk-chan-dongle-16/archive/master.zip
+
+unzip master.zip
+
+rm -f master.zip
+
+cd asterisk-chan-dongle-16-master
+
+
+
+./bootstrap
+./configure --with-astversion=16.2
 make
 make install
+
 cp etc/dongle.conf /etc/asterisk/
 cd ..
-rm -rf asterisk-chan-dongle-asterisk13
+
+rm -rf asterisk-chan-dongle-16-master
 chown asterisk:asterisk /etc/asterisk/dongle.conf
 chmod u=rwX,g=rX,o= /etc/asterisk/dongle.conf
 echo 'KERNEL=="ttyUSB*", MODE="0666", OWNER="asterisk", GROUP="dialout"' > /etc/udev/rules.d/92-dongle.rules
